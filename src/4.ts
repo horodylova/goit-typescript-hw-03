754d0b1de70a59bd8d7881cf653ff4630d529a80
svitlana-horodylova <horodylova.sv@gmail.com>
2024-01-30 23:33:53 +0000

    Added a lot of practise in TypeScript

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,64 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    protected door: boolean = false; 
+    protected key: Key; 
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    abstract openDoor(key: Key): void;
+  
+    comeIn(person: Person) {
+      if (this.door) {
+        console.log(`Person with key signature ${person.getKey().getSignature()} is coming in.`);
+      } else {
+        console.log(`Person with key signature ${person.getKey().getSignature()} can't come in. The door is closed.`);
+      }
+    }
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log('The door is opened.');
+      } else {
+        console.log('Invalid key. The door remains closed.');
+      }
+    }
+  }
+  
+  const key = new Key();
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  house.comeIn(person);
+  
+  
+  
+  
